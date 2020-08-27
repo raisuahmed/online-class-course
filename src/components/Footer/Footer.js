de6f863ea8c286229d578c95ad6cd1c9088bc1ec
raisuahmed <raisuahmed83@gmail.com>
2020-08-28 00:58:04 +0600

    50% work done

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.css'
+const Footer = () => {
+    return (
+        <div className="footer">
+            &copy; DN S Dhrubo | 2020
+        </div>
+    );
+};
+
+export default Footer;
